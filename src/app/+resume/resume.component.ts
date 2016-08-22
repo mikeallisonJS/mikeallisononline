c2fa9fae017bafdec0aedb1ea43287ea67843314
Mike Allison <mike@mikeallisononline.com>
2016-08-22 12:27:34 -0500

    fix formatting

--- src/app/+resume/resume.component.ts
+++ src/app/+resume/resume.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import {MD_GRID_LIST_DIRECTIVES} from "@angular2-material/grid-list";
 
 @Component({
   selector: 'app-resume',
   templateUrl: 'resume.component.html',
-  styleUrls: ['resume.component.scss']
+  styleUrls: ['resume.component.scss'],
+  directives: [MD_GRID_LIST_DIRECTIVES]
 })
 export class ResumeComponent {
   constructor() {}
-	years(time: number) {
-		return (new Date().getFullYear() - time).toString();
-	}
+  years(time: number) {
+    return (new Date().getFullYear() - time).toString();
+  }
 }
