f8a0e1c66d40997e5bcbfb0e078052027c72e04c
Mike Allison <dj.mikeallison@gmail.com>
2017-04-08 23:09:40 -0500

    remove blog logic for now

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,38 @@
 'use strict';
 
+// const http = require('http');
 const functions = require('firebase-functions');
-const feedparser = require('feedparser');
-const request = require('request');
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
-
-exports.feed = functions.https.onRequest((req, res) => {
-    let blogList = [];
-    const url = "http://mikeallisononline.wordpress.com/feed/";
-    const feedreq = request(url);
-    const feed = new feedparser();
-    feedreq.on('error', function (error) {
-      // handle any request errors
-    });
-    feedreq.on('response', function (data) {
-      var stream = this;
-      if (data.statusCode != 200) return this.emit('error', new Error('Bad status code'));
-      stream.pipe(feed);
-    });
-
-    feed.on('error', function(error) {
-      // always handle errors
-    });
-    feed.on('readable', function() {
-      // This is where the action is!
-      var stream = this
-        , meta = this.meta // **NOTE** the "meta" is always available in the context of the feedparser instance
-        , item;
-
-      while (item = stream.read()) {
-        if (blogList.length < 6) {
-          blogList.push({title: item.title, url: item.link});
-        }
-      }
-    });
-    feed.on('end', function() {
-      res.send(blogList);
-    });
-});
+// const FeedParser = require('feedparser');
+//
+// exports.feed = functions.https.onRequest((req, res) => {
+//   let blogList = [];
+//   const options = {
+//     host: 'mikeallisononline.wordpress.com',
+//     path: 'feed/',
+//     port: '443'
+//   };
+//   let feed = new FeedParser();
+//   let wp = http.request(options, (response) => {
+//     response.pipe(feed);
+//   });
+//   wp.end();
+//
+//   feed.on('error', function (error) {
+//     res.status(500).send(error);
+//   });
+//   feed.on('readable', function () {
+//     // This is where the action is!
+//     let stream = this;
+//     let item;
+//
+//     while (item = stream.read()) {
+//       console.log(item);
+//       // if (blogList.length < 6) {
+//       //   blogList.push({title: item.title, url: item.link});
+//       // }
+//     }
+//   });
+//   feed.on('end', function () {
+//     res.send(blogList);
+//   });
+// });
