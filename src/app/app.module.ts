3633ddfbb588e8609e43c452f806ebf69d38e601
Mike Allison <mike@mikeallisononline.com>
2020-02-03 12:54:25 -0800

    angular 4->9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {MaterialModule} from "@angular/material";
@@ -50,7 +50,7 @@ export const firebaseConfig = {
     routing,
     FlexLayoutModule,
     FormsModule,
-    HttpModule,
+    HttpClientModule,
     MaterialModule.forRoot()
   ],
   providers: [],
