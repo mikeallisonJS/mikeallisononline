f26d5bce188e30ab3180f7303766c0fdfa392708
Mike Allison <mike@mikeallisononline.com>
2020-02-03 19:28:01 -0800

    angular 8->9

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import {faCircle} from '@fortawesome/free-solid-svg-icons';
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,11 @@ import {faCircle} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  config?;
+  config: Observable<any>;
   faCircle = faCircle;
   constructor(public af: AngularFireDatabase ) {
   }
   ngOnInit() {
-    this.getConfig();
-  }
-  getConfig() {
     this.config = this.af.object('/config').valueChanges();
   }
 }
